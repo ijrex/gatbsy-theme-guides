5e5c7202e201c5aba9735445c3256c040af737fd
ijrex <iain.rex@googlemail.com>
2020-03-21 00:18:32 +0000

    feat(gatsby-theme-guides): Create GuidePage nodes

--- packages/gatsby-theme-guides/gatsby-config.js
+++ packages/gatsby-theme-guides/gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = options => {
       {
         resolve: 'gatsby-source-filesystem',
         options: {
-          name: 'gatsby-theme-docs',
+          name: 'gatsby-theme-guides',
           path: contentPath,
         },
       },
--- packages/gatsby-theme-guides/gatsby-node.js
+++ packages/gatsby-theme-guides/gatsby-node.js
@@ -12,3 +12,42 @@ exports.onPreBootstrap = (_, options) => {
     mkdirp.sync(dir)
   }
 }
+
+exports.createSchemaCustomization = ({ actions }) => {
+  actions.createTypes(`
+    type GuidePage implements Node @dontInfer {
+      id:  ID!
+      title: String!
+      path: String!
+      updated: Date! @dateformat
+      body: String!
+    }
+  `)
+}
+
+exports.onCreateNode = ({ node, actions, getNode, createNodeId }, options) => {
+  const { basePath } = withDefaults(options)
+  const parent = getNode(node.parent)
+
+  // Only work on MDX files that were loaded by this theme
+  if (
+    node.internal.type !== 'Mdx' ||
+    parent.sourceInstanceName !== 'gatsby-theme-guides'
+  ) {
+    return
+  }
+
+  const pageName = parent.name !== 'index' ? parent.name : ''
+
+  actions.createNode({
+    id: createNodeId(`GuidePage-${node.id}`),
+    title: node.frontmatter.title || parent.name,
+    updated: parent.modifiedTime,
+    path: path.join('/', basePath, parent.relativeDirectory, pageName),
+    parent: node.id,
+    internal: {
+      type: 'GuidePage',
+      contentDigest: node.internal.contentDigest,
+    },
+  })
+}
